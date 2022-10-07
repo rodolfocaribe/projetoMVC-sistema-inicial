fc864f6e14ae5e4e1815a4cd8960f7ce92c4431a
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-10-06 23:23:45 -0300

    consertando inicialização do banco h2

--- src/main/java/sistema/SistemaApplication.java
+++ src/main/java/sistema/SistemaApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-import sistema.repository.Contatos;
+
+import sistema.model.Contato;
 
 import java.util.List;
 
@@ -17,18 +18,16 @@ public class SistemaApplication implements CommandLineRunner {
     }
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    JdbcTemplate db;
 
     @Override
-    public void run(String... args) throws Exception {
-        jdbcTemplate.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "Edson Angoti Júnior", "123", "Rua 1");
-        jdbcTemplate.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "José Joaquim", "123", "Rua 2");
-        jdbcTemplate.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "Maria Carolina", "123", "Rua 3");
-        List<Contatos> contatos = jdbcTemplate.query("SELECT id, nome, telefone FROM contatos", (rs, rowNum) -> {
-            return new Contatos(rs.getInt("id"), rs.getString("nome"), rs.getString("telefone"), rs.getString("endereco"));
-        });
-
+    public void run(String... args) throws Exception {;
+//        db.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "Edson Angoti Júnior", "123",
+//                "Rua 1");
+//        db.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "José Joaquim", "123",
+//                "Rua 2");
+//        db.update("INSERT INTO contatos(nome,telefone,endereco) VALUES (?,?,?)", "Maria Carolina", "123",
+//                "Rua 3");
     }
-
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+create table if not exists contatos (
+  id serial,
+  nome varchar(50),
+  telefone varchar(20),
+  endereco varchar(50),
+  primary key (id)
+);
+insert into contatos (nome,telefone,endereco) VALUES ('Rodrigo Faro', '123456789', 'Rua dos Bobos, 40');
+insert into contatos (nome,telefone,endereco) VALUES ('Fernanda Lima', '987654321', 'Rua dos engraçados, 50');
+insert into contatos (nome,telefone,endereco) VALUES ('Fausto Silva', '123456789', 'Rua dos Bobos, 40');
