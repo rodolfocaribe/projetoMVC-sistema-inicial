81e04463effed45315ceb0ecfdbe36a5de231014
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-10-07 21:12:20 -0300

    implementa reomção e edição de alunos

--- src/main/java/sistema/controller/HomeController.java
+++ src/main/java/sistema/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import sistema.model.Aluno;
 import sistema.model.Professor;
 
@@ -79,6 +80,37 @@ public class HomeController {
         db.update("insert into professores (nome) values (?)",
                 professor.getNome());
         return "redirect:/professores";
+    }
+    @GetMapping ("excluir-aluno")
+    public String apagarContato(@RequestParam(value = "id", required = true) Integer cod) {
+        db.update("delete from alunos where id=?", cod);
+        return "redirect:/alunos";
+    }
+
+    @GetMapping ("editar-aluno")
+    public String exibeFormAlteracaoAluno(@RequestParam (value = "id", required = true) Integer cod, Model model){
+        Aluno aluno = db.queryForObject(
+                "select * from alunos where id = ?",
+                (rs, rowNum) -> {
+                    Aluno edited = new Aluno();
+                    edited.setId(rs.getInt("id"));
+                    edited.setNome(rs.getString("nome"));
+                    edited.setEndereco(rs.getString("endereco"));
+                    edited.setTelefone(rs.getString("telefone"));
+                    return edited;
+                }, cod);
+        model.addAttribute("alunoEditado", aluno );
+        return "editaaluno";
+    }
+    @PostMapping("gravaalunoeditado")
+    public String gravaAlunoEditado(Aluno aluno) {
+        db.update(
+                "update alunos set nome=?, telefone=?, endereco=? where id = ?",
+                aluno.getNome(),
+                aluno.getTelefone(),
+                aluno.getEndereco(),
+                aluno.getId());
+        return "redirect:/alunos";
     }
 }
 
